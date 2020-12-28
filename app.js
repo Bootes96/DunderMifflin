84f596622987aca00c80ce74329f1106089292d8
Bootes <andreyantman@gmail.com>
2020-12-28 22:51:11 +0300

    Сохранение товаров в корзине и localStorage, подсчет общей стоимости

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const cartItems = document.querySelector('.cart__items')
 const cartTotal = document.querySelector('.cart__total')
 const cartContent = document.querySelector('.cart__content')
 const productsDOM = document.querySelector('.products__content')
+const busketItems = document.querySelector('.busket__items')
 
 let cart = []
 //Buttons
@@ -83,19 +84,80 @@ class UI {
         const product = new singleProduct()
         const result = await product.getSingleProduct(id)
         const cartItem = {...result, amount: 1}
-        cart = [...cart, cartItem]   
-        console.log(cart)
+        //add product to the cart
+        cart = [...cart, cartItem]
+        //save the cart in local storage   
+        Storage.saveCart(cart)
+        //set cart values
+        this.setCartValues(cart)
+        //display cart item
+        this.addCartItem(cartItem) 
+        //show the cart after adding product
+        this.showCart()
+    }
+    setCartValues(cart) {
+        let tempTotal = 0
+        let itemsTotal = 0
+        cart.map(item => {
+            tempTotal += item.price * item.amount
+            itemsTotal += item.amount
+        })
+        cartTotal.innerText = parseFloat(tempTotal.toFixed(2))
+        busketItems.innerHTML = itemsTotal
+    }
+    addCartItem(item) {
+        const div = document.createElement('div')
+        div.classList.add('cart__item')
+        div.innerHTML = `
+            <img src=${item.image} alt="product">
+            <div>
+                <h4>${item.title}</h4>
+                <h5>$${item.price}</h5>
+                <span class="item__remove">remove</span>
+            </div>
+            <div>
+                <i class="fas fa-chevron-up" data-id=${item.id}></i>
+                <p class="item__amount">${item.amount}</p>
+                <i class="fas fa-chevron-down" data-id=${item.id}></i>
+            </div>
+        `
+        cartContent.appendChild(div)
+    }
+    showCart() {
+        cartOverlay.classList.add('transparentBcg')
+        cartDOM.classList.add('showCart')
+    }
+    hideCart() {
+        cartOverlay.classList.remove('transparentBcg')
+        cartDOM.classList.remove('showCart')
+    }
+    setupApp() {
+        cart = Storage.getCart()
+        this.setCartValues(cart)
+        this.populateCart(cart)
+        busket.addEventListener('click', this.showCart)
+        cartCloseBtn.addEventListener('click', this.hideCart)
+    }
+    populateCart(cart) {
+        cart.forEach(item => this.addCartItem(item))
     }
 }
 
 //Local Storage
 class Storage {
-
+    static saveCart(cart) {
+        localStorage.setItem('cart', JSON.stringify(cart))
+    }
+    static getCart() {
+        return localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : []
+    }
 } 
 
 document.addEventListener('DOMContentLoaded', () => {
     const products = new Products()
     const ui = new UI()
+
+    ui.setupApp()
 
     products.getProducts().then(products => {
         ui.displayProducts(products)
